14fe5b555e9e10c879d3b4b22c4128cd5cb20420
Hardiegit10 <kolawole.adedeji10@gmail.com>
2023-07-15 09:32:52 +0100

    Update countdown date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("May 27, 2023 23:59:59").getTime();
+var countDownDate = new Date("July 23, 2023 23:59:59").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
